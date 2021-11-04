8d5e0eb70cd54f8402fd6735a9bee941c4b4102f
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-04 19:02:32 +0400

    Create Homework 14.1.cpp

---
+++ Homework 14.1/Homework 14.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int tableware[2][6] = { {4,3,3,3,3,3},{4,3,3,3,3,3} };
+    int dishes[2][6] = { {3,2,2,2,2,2},{3,2,2,2,2,2} };
+    int chairs[2][6] = { {1,1,1,1,1,1},{1,1,1,1,1,1} };
+
+    chairs[0][4]++;
+    tableware[1][2]--;
+    tableware[1][0]--;
+    tableware[1][2]++;
+    dishes[1][0]--;
+
+}
+
