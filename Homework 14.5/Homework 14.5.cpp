0583cd025edc7dae32389a39a59fe08c9c640691
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-05 01:14:53 +0400

    Create Homework 14.5.cpp

---
+++ Homework 14.5/Homework 14.5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float M[4][4];
+    std::cout << "Enter numbers of matrix M:";
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cin >> M[i][j];
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (i != j) M[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << M[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
