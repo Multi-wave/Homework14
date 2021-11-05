cfcb01b731ea285b6097ac9f4c97ef23dae1f876
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-05 22:02:24 +0400

    Create Homework14.6.cpp

---
+++ Homework14.6/Homework14.6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector <float> V;
+    float M[4][4];
+    vector <float> R;
+    float temp;
+
+    cout << "Enter a numbers of V: ";
+    for (int i = 0; i < 4; ++i) {
+        cin >> temp;
+        V.push_back(temp);
+    }
+    cout << "Enter a numbers of M: ";
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cin >> M[i][j];
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        temp = 0;
+        for (int j = 0; j < 4; ++j) {
+            temp += V[j] * M[j][i];
+        }
+        R.push_back(temp); 
+    }
+    for (int i = 0; i < 4; ++i) {
+        cout << R[i] << " ";
+    }
+}
